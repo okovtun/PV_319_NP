b8ed53401ee36c5e8ec38153587682b40fe6f023
User <User@DESKTOP-QHG18FL>
2025-06-07 19:46:26 +0300

    Extra clients receive SORRY Message

--- NetworkProgramming/WinSockClient/main.cpp
+++ NetworkProgramming/WinSockClient/main.cpp
@@ -15,6 +15,7 @@ using namespace std;
 
 #define DEFAULT_PORT	"27015"
 #define DEFAULT_BUFFER_LENGTH	1500
+#define SZ_SORRY  "Sorry, but all is busy"
 
 void main()
 {
@@ -84,6 +85,9 @@ void main()
 		if (iResult > 0)cout << "Bytes received: " << iResult << ", Message: " << recvbuffer << endl;
 		else if (iResult == 0)cout << "Connection closed" << endl;
 		else cout << "Receive failed with code: " << WSAGetLastError() << endl;
+		////////////////////////////////////////////////
+		if (strcmp(recvbuffer, SZ_SORRY) == 0)break;
+		////////////////////////////////////////////////
 		ZeroMemory(send_buffer, sizeof(send_buffer));
 		ZeroMemory(recvbuffer, sizeof(recvbuffer));
 		cout << "Введите сообщение: "; 
